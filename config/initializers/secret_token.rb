0cd72777e4075d777e60aedbaa59c7dcfc5299d2
Evan McEwen <evan@evanmcewen.ca>
2012-06-11 19:34:00 -0400

    Inital rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smswebapp::Application.config.secret_token = '720b00eecd08dfbba8cb9133ed2049648d98f0e7129141548c024898fe49e28b619c5a9e737466da33962a543682d36749387f494494f6f3d2b835cd75de7782'
