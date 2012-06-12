157f0b1aed3cb30912bb7a284bb83c97aa7374ec
Evan McEwen <evan@evanmcewen.ca>
2012-06-11 21:10:18 -0400

    Missing files lol

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+  has_secure_password
+  
+  attr_accessible :email, :password, :password_confirmation
+  
+  def self.authenticate(user, pass)
+    user = User.where(:username => user).first.try(:authenticate, pass)
+  end
+  
+  private
+  validates :username, :length => { :in => 3..20 }, :uniqueness => true, :presence => true
+  validates :email, :format => { :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i }, :uniqueness => true
+end
