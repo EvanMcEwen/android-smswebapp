a4ec24c1c89df959337e35ef043c644f07e267c5
Evan McEwen <evan@evanmcewen.ca>
2012-06-11 21:05:22 -0400

    Added user authentication/model and bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Smswebapp::Application.routes.draw do
+  
+  resources :users
+
+  root :to => 'home#index'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
