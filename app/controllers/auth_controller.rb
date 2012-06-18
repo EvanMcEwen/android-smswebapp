6294d9800aa9ed5ce12bd6a7a36ac52a62c90ce1
Evan McEwen <evan@evanmcewen.ca>
2012-06-18 12:54:20 -0400

    Added mising before filter

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,5 +1,5 @@
 class AuthController < ApplicationController
-  skip_before_filter :ensure_user_logged_in, :only => [:login, :logout]
+  skip_before_filter :ensure_user_logged_in, :only => [:login, :logout, :mobile_login]
   
   def login
     session[:user_id] = User.authenticate(params[:username], params[:password]) if params[:username]
