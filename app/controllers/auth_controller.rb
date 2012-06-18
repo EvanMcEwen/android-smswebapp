a8c55b433f7eb6dbe2c311066ab9ec8cea5ab984
Evan McEwen <evan@evanmcewen.ca>
2012-06-18 12:13:30 -0400

    Fixed blank page on login

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,15 +2,17 @@ class AuthController < ApplicationController
   skip_before_filter :ensure_user_logged_in, :only => [:login, :logout]
   
   def login
-    session[:user_id] = User.authenticate(params[:username], params[:password]) if params[:username]
-    update_session_expiry
-    
-    respond_to do |format|
-      if !session[:user_id].nil?
-        format.html { redirect_to '/' }
-        format.json { render :json => {:status => 1} }
-      else
-        format.json { render :json => {:status => 0} }
+    if params[:username]
+      session[:user_id] = User.authenticate(params[:username], params[:password])
+      update_session_expiry
+      
+      respond_to do |format|
+        if !session[:user_id].nil?
+          format.html { redirect_to '/' }
+          format.json { render :json => {:status => 1} }
+        else
+          format.json { render :json => {:status => 0} }
+        end
       end
     end
   end
