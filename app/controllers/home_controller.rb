13c31cbc12a9c1ebe1f1a41acd60ac257d7262d6
Evan McEwen <evan@evanmcewen.ca>
2012-07-24 20:01:58 -0400

    Testing conversaton logic

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,17 @@
 class HomeController < ApplicationController
   def index
+  	@conversations = get_conversations()
   end
 
+
+  private
+  def get_conversations()
+  	messages = Message.find_all_by_user_id(session[:user_id], :order => "timestamp DESC")
+
+  	contacts = []
+  	for msg in messages
+  		contacts.push(msg.origin) if !contacts.include?(msg.origin) && !msg.origin.eql?("DEVICE")
+  	end
+  	return contacts
+  end
 end
