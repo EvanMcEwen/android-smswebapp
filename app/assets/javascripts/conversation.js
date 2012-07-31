57bac16ee044b4d7af5d0ca630368f3bc9166c49
Evan McEwen <evan@evanmcewen.ca>
2012-07-31 19:25:58 -0400

    Minor fix

--- app/assets/javascripts/conversation.js
+++ app/assets/javascripts/conversation.js
@@ -3,7 +3,7 @@ function switchConversation(x)
 	$.get('messages/' + x, function(data) {
 	  $('#well-conversation').html(data);
 	  $('#well-conversation').attr('class', 'well-conversation');
-	  $('#h2-conversation').html("Conversation with " + x);
+	  $('#h2-conversation').html("Conversation with <b>" + x + "</b>");
 	  $('.messages').scrollTop(99999);
 	});
 }
