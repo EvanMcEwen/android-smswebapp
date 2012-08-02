db4a7e849ca20d70e105f33fc6fd31ed9a865a72
Evan McEwen <evan@evanmcewen.ca>
2012-08-02 16:59:57 -0400

    Added active styling to sidebar

--- app/assets/javascripts/conversation.js
+++ app/assets/javascripts/conversation.js
@@ -1,8 +1,21 @@
+var prevActive = false;
+
 function switchConversation(x)
 {
 	$.get('messages/' + x, function(data) {
 	  $('#well-conversation').html(data);
 	  $('#well-conversation').attr('class', 'well-conversation');
+	  if (!prevActive)
+	  {
+	  	$('#' + x + '_nav').attr('class', 'active');
+	  	prevActive = '#' + x + '_nav';
+	  }
+	  else
+	  {
+	  	$('#' + x + '_nav').attr('class', 'active');
+	  	$(prevActive).removeClass('active');
+	  	prevActive = '#' + x + '_nav';	
+	  }
 	  $('#h2-conversation').html("Conversation with <b>" + x + "</b>");
 	  $('.messages').scrollTop(99999);
 	});
