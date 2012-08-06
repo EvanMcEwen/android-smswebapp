cb551a4e33fbe4a7d504cd2254a353d1531060c0
Evan McEwen <evan@evanmcewen.ca>
2012-08-06 15:46:02 -0400

    Search and conversation viewing

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,16 @@
 class HomeController < ApplicationController
   def index
-  	@conversations = get_conversations()
+    @conversations = get_conversations(params[:number])
   end
 
 
   private
-  def get_conversations()
-  	messages = Message.find_all_by_user_id(session[:user_id], :order => "timestamp DESC")
+  def get_conversations(number)
+    if (number.nil?)
+      messages = Message.find_all_by_user_id(session[:user_id], :order => "timestamp DESC")
+    else
+      messages = Message.find_all_by_user_id(session[:user_id], :conditions => ["origin = ? OR destination = ?",number,number], :order => "timestamp DESC")
+    end
 
   	contacts = []
   	for msg in messages
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,6 @@
 class SearchController < ApplicationController
   def lookfor
   	query = params[:searchQuery]
+  	@results = Message.find_all_by_user_id(session[:user_id], :conditions => ["message LIKE ?","%" + query + "%"], :order => "timestamp DESC")
   end
 end
